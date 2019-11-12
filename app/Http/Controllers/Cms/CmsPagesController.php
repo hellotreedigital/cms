2d125e4f818b31869c4a2d4d4f3946c69c581195
Ghady Khoury <ghady@hellotree.co>
2019-11-12 17:36:59 +0200

    Update CmsPagesController.php

--- app/Http/Controllers/Cms/CmsPagesController.php
+++ app/Http/Controllers/Cms/CmsPagesController.php
@@ -922,18 +922,16 @@ class CmsPagesController extends Controller
 		file_put_contents(
 			base_path('routes/cms.php'),
 			str_replace(
-				"
+				[
+					"
 	Route::resource('/" . $cms_page->route . "', 'Cms\\" . $cms_page->controller_name . "');",
-				"",
-				$web_routes
-			)
-		);
-		file_put_contents(
-			base_path('routes/cms.php'),
-			str_replace(
-				"
+					"
 	Route::get('/" . $cms_page->route . "/order', 'Cms\\" . $cms_page->controller_name . "@orderIndex');Route::post('/" . $cms_page->route . "/order', 'Cms\\" . $cms_page->controller_name . "@orderSubmit');Route::resource('/" . $cms_page->route . "', 'Cms\\" . $cms_page->controller_name . "');",
-				"",
+				],
+				[
+					"",
+					"",
+				],
 				$web_routes
 			)
 		);
