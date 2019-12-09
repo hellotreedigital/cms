71ed4e5814eb36c442e4faad63bfb846f9c1265d
Ghady Khoury <ghady.kh@gmail.com>
2019-12-09 13:51:05 +0200

    git fix

--- src/providers/CmsServiceProvider.php
+++ src/providers/CmsServiceProvider.php
@@ -9,20 +9,35 @@ Class CmsServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
+		// Routes
 		include __DIR__ . '/../routes.php';
-		$this->publishes([__DIR__ . '/../assets' => public_path('cms/')], 'cms_assets');
+		
+		// Publish cms assets
+		$this->publishes(
+			[
+				__DIR__ . '/../assets' => public_path('cms/'),
+				__DIR__ . '/../models' => app_path('/'),
+			],
+		'cms_assets');
+
 		Artisan::call('vendor:publish --tag=cms_assets --force');
+	    
+		// Migrations
 	    $this->loadMigrationsFrom(__DIR__ . '/../migrations');
 	}
 
 	public function register()
 	{
+		// Controllers
 		$this->app->make('Hellotreedigital\Cms\Controllers\AdminRolesController');
 		$this->app->make('Hellotreedigital\Cms\Controllers\AdminsController');
 		$this->app->make('Hellotreedigital\Cms\Controllers\CmsController');
 		$this->app->make('Hellotreedigital\Cms\Controllers\CmsPagesController');
 
+		// Views
 		$this->loadViewsFrom(__DIR__ . '/../views', 'cms');
+		
+		// Admin middleware
 		$this->app['router']->aliasMiddleware('admin', \Hellotreedigital\Cms\Middlewares\AdminPermissions::class);
 	}
 }
