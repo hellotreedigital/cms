e9d157879c3da57748da7b48dc36167b2b37e75c
Hellotree <58079961+hellotreedigital@users.noreply.github.com>
2020-02-07 15:53:33 +0200

    Update select-multiple.blade.php

--- src/views/cms/components/form-fields/select-multiple.blade.php
+++ src/views/cms/components/form-fields/select-multiple.blade.php
@@ -1,8 +1,20 @@
+@php
+if ($errors->any()) {
+	$selected = old($name) ? old($name) : [];
+} else {
+	if (!isset($value) || !$value) $value = [];
+	$selected = [];
+	foreach ($value as $obj) {
+		$selected[] = $obj[$store_column];
+	}
+}
+@endphp
+
 <div class="form-group">
 	<label class="d-block">{{ $label }}</label>
-	<select class="form-control" name="{{ $name }}" multiple="">
+	<select class="form-control" name="{{ $name }}[]" multiple="">
 		@foreach($options as $option)
-			<option value="{{ $option[$store_column] }}" {{ isset($value) && is_array($value) && in_array($option[$store_column], $value) ? 'selected' : '' }}>
+			<option value="{{ $option[$store_column] }}" {{ in_array($option[$store_column], $selected) ? 'selected' : '' }}>
 				{{ $option[$display_column] }}
 			</option>
 		@endforeach
