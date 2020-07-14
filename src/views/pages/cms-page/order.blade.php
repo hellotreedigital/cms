81de57530ec4c3237cac219df27aa071c9ed0480
Hellotree <58079961+hellotreedigital@users.noreply.github.com>
2020-07-14 12:42:38 +0300

    Update order.blade.php

--- src/views/pages/cms-page/order.blade.php
+++ src/views/pages/cms-page/order.blade.php
@@ -16,23 +16,41 @@
 				<ul class="sortable list-inline m-0">
 					@foreach($rows as $row)
 						<li class="sortable-row d-block bg-white border px-3 py-2 mb-2">
-							<input type="hidden" name="ht_pos[{{ $row['id'] }}]" value="{{ $row['ht_pos'] }}">
-							@foreach($page_fields as $field)
-								@if ($field['name'] == $page['order_display'])
-									@if ($field['form_field'] == 'image')
-										<img src="{{ asset($row[$page['order_display']]) }}">
-									@elseif ($field['form_field'] == 'select')
-										{{ $row[str_replace('_id', '', $field['name'])][$field['form_field_additionals_2']] }}
-									@elseif ($field['form_field'] == 'select multiple')
-										@foreach($row[$field['form_field_additionals_1']] as $i => $second_table_row)
-											@if ($i) , @endif
-											{{ $second_table_row[$field['form_field_additionals_2']] }}
-										@endforeach
-									@else
-										{{ $row[$page['order_display']] }}
-									@endif
-								@endif
-							@endforeach
+                            <input type="hidden" name="ht_pos[{{ $row['id'] }}]" value="{{ $row['ht_pos'] }}">
+                            @php
+                                $order_field = null;
+                                foreach($page_fields as $field) {
+                                    if ($field['name'] == $page['order_display']) {
+                                        $order_field = $field;
+                                        break;
+                                    }
+                                }
+
+                                if (!$order_field) {
+                                    foreach($page_translatable_fields as $field) {
+                                        if ($field['name'] == $page['order_display']) {
+                                            $order_field = $field;
+                                            break;
+                                        }
+                                    }
+                                }
+                            @endphp
+                            @if ($order_field)
+                                @if ($order_field['name'] == $page['order_display'])
+                                    @if ($order_field['form_field'] == 'image')
+                                        <img src="{{ asset($row[$page['order_display']]) }}">
+                                    @elseif ($order_field['form_field'] == 'select')
+                                        {{ $row[str_replace('_id', '', $order_field['name'])][$order_field['form_field_additionals_2']] }}
+                                    @elseif ($order_field['form_field'] == 'select multiple')
+                                        @foreach($row[$order_field['form_field_additionals_1']] as $i => $second_table_row)
+                                            @if ($i) , @endif
+                                            {{ $second_table_row[$order_field['form_field_additionals_2']] }}
+                                        @endforeach
+                                    @else
+                                        {{ $row[$page['order_display']] }}
+                                    @endif
+                                @endif
+                            @endif
 						</li>
 					@endforeach
 				</ul>
