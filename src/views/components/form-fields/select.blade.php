956b15dbc1b36f002fd3fa77bfd90863397ed911
Ghady Khoury <ghady.kh@gmail.com>
2020-08-25 13:28:07 +0300

    fixed dropdown on richtextbox

--- src/views/components/form-fields/select.blade.php
+++ src/views/components/form-fields/select.blade.php
@@ -4,8 +4,9 @@
 <div class="form-group">
 	@include('cms::components/form-fields/label')
 	<select class="form-control" name="{{ $name }}">
+		<option></option>
 		@foreach($options as $option)
-			<option value="{{ $option[$store_column] }}" {{ $value == $option[$store_column] ? 'selected' : '' }}>{{ $option[$display_column] }}</option>
+			<option value="{{ $option[$store_column] }}" {{ $value == $option[$store_column] ? 'selected' : '' }}>{{ strip_tags($option[$display_column]) }}</option>
 		@endforeach
 	</select>
 </div>
